ae9bbd97bc5d9b5b35125633e4fecc68e8802c96
Alejandro Migallón <139125973+alexmrDev@users.noreply.github.com>
2023-10-08 01:46:14 +0200

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+/* NAVBAR SCROLLS */
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener("click", () => {
+  scrollToElement(".header");
+});
+
+link2.addEventListener("click", () => {
+  scrollToElement(".header", 1);
+});
+
+link3.addEventListener("click", () => {
+  scrollToElement(".column");
+});
+
+/* TEXT ANIMATION */
+const separator = document.querySelector(".separator");
+const spanInsideSeparator = separator.querySelector("span");
+const desplegable = document.querySelector(".desplegable");
+
+let spanVisible = true;
+
+separator.addEventListener("click", () => {
+  desplegable.classList.toggle("open");
+  spanInsideSeparator.style.display = "none";
+
+  if (spanVisible) {
+    spanInsideSeparator.style.display = "none";
+  } else {
+    spanInsideSeparator.style.display = "inline";
+  }
+
+  spanVisible = !spanVisible;
+});
